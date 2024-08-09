a86907c8b80d6ed369de39e99ca7b16c4386752f
1774293824 <95674405+1774293824@users.noreply.github.com>
2024-08-09 12:14:59 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -2,7 +2,7 @@
 sed -i 's/192.168.1.1/192.168.68.1/g' package/base-files/files/bin/config_generate
 
 # Modify default theme（设置默认主题）
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname（设置设备名称）本地编译时在文件的第298行左右
 sed -i 's/OpenWrt/QWrt/g' package/base-files/files/bin/config_generate
